f87379c23424b078efa4fdd2d38575425cbcaa58
kyo1941 <kyo1941@outlook.jp>
2025-06-26 03:43:02 +0900

    fix: ダークモード時にステータスバーを表示

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/theme/Theme.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package jp.co.yumemi.android.code_check.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
@@ -7,6 +8,11 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple200,
@@ -30,6 +36,14 @@ fun AppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
     MaterialTheme(
         colorScheme = colorScheme,
         typography = androidx.compose.material3.Typography()
