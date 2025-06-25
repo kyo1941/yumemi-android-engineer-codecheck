2681f1ab0585fac8664ff1ba845a3968ce68402c
kyo1941 <kyo1941@outlook.jp>
2025-06-25 23:34:22 +0900

    refactor: 不要なインポート文の削除

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/di/AppModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/di/AppModule.kt
@@ -6,7 +6,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.android.Android
 import jp.co.yumemi.android.code_check.data.api.GitHubApiClient
 import jp.co.yumemi.android.code_check.data.repository.GitHubRepositoryImpl
 import jp.co.yumemi.android.code_check.domain.repository.GitHubRepository
